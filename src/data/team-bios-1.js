c5407fa93ac376e06aa692e526feac4bd08aefc5
John <johnhpark@berkeley.edu>
2021-01-20 18:32:40 -0800

    Update placeholder team pics with ASUC logo

--- src/data/team-bios-1.js
+++ src/data/team-bios-1.js
@@ -4,20 +4,20 @@ export default [
       '',
     name: 'Apoorva Prakash',
     // title: 'UC Berkeley ASUC Senator',
-    picture: 'https://placeimg.com/150/150/people'
+    picture: 'https://dev.asuc.org/wp-content/uploads/2016/10/all.png'
   },
   {
     // bio:
     //   'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
     name: 'John Park',
     // title: 'Director of Research and Data',
-    picture: 'https://placeimg.com/150/150/people'
+    picture: 'https://dev.asuc.org/wp-content/uploads/2016/10/all.png'
   },
   {
     // bio:
     //   'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
     name: 'Allison Jung',
     // title: 'Director of Research and Data',
-    picture: 'https://placeimg.com/150/150/people'
+    picture: 'https://dev.asuc.org/wp-content/uploads/2016/10/all.png'
   },
 ];
--- src/data/team-bios-2.js
+++ src/data/team-bios-2.js
@@ -4,20 +4,20 @@ export default [
     //   'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
     name: 'Jessica Situ',
     // title: 'Director of Research and Data',
-    picture: 'https://placeimg.com/150/150/people'
+    picture: 'https://dev.asuc.org/wp-content/uploads/2016/10/all.png'
   },
   {
     // bio:
     //   'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
     name: 'Zoe Ko',
     // title: 'Director of Research and Data',
-    picture: 'https://placeimg.com/150/150/people'
+    picture: 'https://dev.asuc.org/wp-content/uploads/2016/10/all.png'
   },
   {
     // bio:
     //   'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
     name: 'Minju Kang',
     // title: 'Director of Research and Data',
-    picture: 'https://placeimg.com/150/150/people'
+    picture: 'https://dev.asuc.org/wp-content/uploads/2016/10/all.png'
   },
 ];
